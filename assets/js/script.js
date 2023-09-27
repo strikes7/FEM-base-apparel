0764b42c95fda16f2847a58060a46cc01ef0bbb5
matias <strike.s7@outlook.com>
2023-09-27 16:38:55 -0300

    practice completed

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+window.onload = addEvents();
+
+function addEvents() {    
+    document.getElementById("newsletter-send").addEventListener('click',validateEmail
+    );
+}
+
+function validateEmail(event){
+    event.preventDefault()
+    
+    const regExEmail = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
+    const email = document.getElementById("newsletter-email");
+    const form = document.getElementById("newsletter-form");
+    
+    if (regExEmail.test(email.value)){
+        form.classList.remove("error");
+        return true;
+    } else{
+        form.classList.add("error");
+        return false;
+    }
+}
